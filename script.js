6ad318f37ba0cb613d8815db6f07060d6353c70a
berni23 <bernatferrerm@gmail.com>
2020-08-01 19:26:18 +0200

    game-box moves without delay

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ colors.forEach(color => {
 })
 
 function nextStepForm() {
-
     displayLetters(arrSteps[stepForm + 1], formTitle);
     stepForm++;
 }
@@ -51,9 +50,7 @@ function displayLetters(charArray, element) {
 }
 
 function createUser(newUser) {
-
     newUser = newUser.trim();
-
     let user = {
         username: newUser,
         mode: "EASY",
@@ -66,33 +63,25 @@ function createUser(newUser) {
 }
 
 for (let i = 0; i < childBtnsMode.length; i++) {
-
     childBtnsMode[i].addEventListener("click", updateMode);
-
 }
 
 function updateMode(event) {
-
     let currentMode = event.target.textContent;
     Users[currentUser].mode = currentMode;
-
 }
 
 function updateColor(event) {
-
     let currentColor = event.target.style.backgroundColor;
     Users[currentUser].color = currentColor;
-
 }
 
 btnForm.addEventListener("click", validateForm);
 btnReady.addEventListener("click", validateForm);
 
 function validateForm() {
-
     switch (stepForm) {
-
-        case 0: {
+        case 0: { // username
 
             let username = inputName.value;
             if (username != "") {
@@ -100,65 +89,101 @@ function validateForm() {
                 createUser(username);
                 showStep1Form();
                 nextStepForm();
-
             }
-
             break;
         }
-        case 1: {
+        case 1: { //choose color
 
             showStep2Form();
             nextStepForm();
             break;
         }
-
-        case 2: {
+        case 2: { // are u ready?
 
             nextStepForm();
             showStep3Form();
-
+            console.log("are u ready?")
+            break;
         }
 
-        case 3: {
+        case 3: { // start game
             startGame();
+            break;
         }
     }
-
 }
 
 function showStep1Form() {
-
     inputName.classList.add("hidden");
     buttonsMode.classList.remove("hidden");
-
 }
 
 function showStep2Form() {
-
     buttonsMode.classList.add("hidden");
     carouselColor.classList.remove("hidden");
 
 }
 
 function showStep3Form() {
-
-
     carouselColor.classList.add("hidden");
     btnForm.classList.add("hidden");
     btnReady.classList.remove("hidden");
-
 }
 
-
-
 function startGame() {
-
     mainSection.classList.add("hidden");
     gameSection.classList.remove("hidden")
+    document.onkeydown = detectKey;
+    document.onkeyup = removeKey;
 
+    keyLoop();
     // set a counter with 1 2 3 in the screen
 }
 
 /* start form completion */
 
 nextStepForm();
+
+
+/*experiment with the game*/
+
+var currentKey;
+
+function detectKey(e) {
+
+    if (e.keyCode == '87' || e.keyCode == '83' || e.keyCode == '65' || e.keyCode == '68') {
+        currentKey = e.keyCode
+    }
+}
+
+function removeKey(e) {
+
+
+    console.log("inside removeKey function")
+
+    if (e.keyCode == currentKey) {
+        currentKey = undefined;
+
+        console.log("currentKey removed");
+    }
+}
+
+function keyLoop() {
+    var posLeft = document.getElementById('myId').offsetLeft;
+    var posTop = document.getElementById('myId').offsetTop;
+    if (currentKey == '87') {
+        // up arrow
+        document.getElementById('myId').style.marginTop = (posTop - 5) + "px";
+    } else if (currentKey == '83') {
+        // down arrow
+        document.getElementById('myId').style.marginTop = (posTop + 5) + "px";
+    } else if (currentKey == '65') {
+        // left arrow
+        document.getElementById('myId').style.marginLeft = (posLeft - 5) + "px";
+    } else if (currentKey == '68') {
+        // right arrow
+        document.getElementById('myId').style.marginLeft = (posLeft + 5) + "px";
+    }
+
+    setTimeout(keyLoop, 10);
+}
