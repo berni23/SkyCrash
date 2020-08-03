c275ae53f17749393c1c3bec867b03c9eb3e45ae
berni23 <bernatferrerm@gmail.com>
2020-08-03 13:19:43 +0200

    array of consecutive obstacles created

--- script.js
+++ script.js
@@ -81,7 +81,6 @@ btnReady.addEventListener("click", validateForm);
 function validateForm() {
     switch (stepForm) {
         case 0: { // username
-
             let username = inputName.value;
             if (username != "") {
 
@@ -122,6 +121,7 @@ function showStep2Form() {
     carouselColor.classList.remove("hidden");
 
 }
+"1"
 
 function showStep3Form() {
     carouselColor.classList.add("hidden");
@@ -134,127 +134,151 @@ function startGame() {
     gameSection.classList.remove("hidden")
     document.onkeydown = detectKey;
     document.onkeyup = removeKey;
+    setTimeout(pushObstacle, arrObs[0][0])
+
     keyLoop();
     // set a counter with 1 2 3 in the screen
 }
 
 /* start form completion */
-
 nextStepForm();
-
-
 /* game logic implementation*/
 
 var currentKey;
 
 function detectKey(e) {
-
     if (e.keyCode == '87' || e.keyCode == '83' || e.keyCode == '65' || e.keyCode == '68') {
         currentKey = e.keyCode
     }
 }
 
 function removeKey(e) {
-
     if (e.keyCode == currentKey) {
         currentKey = undefined;
     }
 }
 
-let collider = document.getElementById('collider');
-let box = document.getElementById('myId');
-
-
+//let collider = document.getElementById('collider');
+let box = document.getElementById("myId");
+console.log(box);
 // game constraints
-
 const gWidth = 1500;
 const gHeight = 500;
 const boxSize = pixToInt(box.style.width);
-console.log("boxSize", boxSize);
 const marginGame = 5;
 const limitBottom = gHeight - boxSize - marginGame;
 const limitRight = gWidth - boxSize - marginGame;
 
-collider.style.marginLeft = "750px";
-collider.style.marginTop = "250px";
-//box.style.marginLeft = "750px";
-//box.style.marginTop = "250px";
-
+box.style.marginLeft = "750px";
+box.style.marginTop = "250px";
 
 function keyLoop() {
-    let posLeft = document.getElementById('myId').offsetLeft;
-    let posTop = document.getElementById('myId').offsetTop;
-    let posLeft2 = document.getElementById('collider').offsetLeft;
-    let posTop2 = document.getElementById('collider').offsetTop;
+    let posLeft = box.offsetLeft;
+    let posTop = box.offsetTop;
+    //let posTop2 = collider.offsetTop;
+    if (currentKey == '87' && borderTop(box)) {
+        box.style.marginTop = (posTop - 5) + "px";
+    } else if (currentKey == '83' && borderBottom(box)) {
+        box.style.marginTop = (posTop + 5) + "px";
+    } else if (currentKey == '65' && borderLeft(box)) {
+        box.style.marginLeft = (posLeft - 5) + "px";
+    } else if (currentKey == '68' && borderRight(box)) {
+        box.style.marginLeft = (posLeft + 5) + "px";
+    }
+    //collision(box, collider);
+    for (el of currentObs) {
 
-    if (currentKey == '87' && borderTop()) {
-        // up arrow
-        document.getElementById('myId').style.marginTop = (posTop - 5) + "px";
-        console.log("red", posLeft, posTop);
-        console.log("blue", posLeft2, posTop2);
+        if (borderLeft(el)) {
+            el.style.marginLeft = (el.offsetLeft - 5) + "px";
 
-        collision(document.getElementById('myId'), document.getElementById('collider'))
+        } else {
 
-    } else if (currentKey == '83' && borderBottom()) {
-        // down arrow
-        document.getElementById('myId').style.marginTop = (posTop + 5) + "px";
-    } else if (currentKey == '65' && borderLeft()) {
-        // left arrow
-        document.getElementById('myId').style.marginLeft = (posLeft - 5) + "px";
-    } else if (currentKey == '68' && borderRight()) {
-        // right arrow
-        document.getElementById('myId').style.marginLeft = (posLeft + 5) + "px";
+            el.remove();
+        }
     }
 
     setTimeout(keyLoop, 10);
 }
 
+/* create obstacles */
+
+var numObs = 0;
+var arrObs = [
+    [3000, "250px"],
+    [2000, "250px"],
+    [1000, "310px"]
+];
+
+var currentObs = [];
+
+function pushObstacle() {
+
+    let blueBox = document.createElement('div');
+    blueBox.classList.add("blue-box");
+    /*blueBox.style.backgroundColor = "blue";
+    blueBox.classList.add("box");
+    blueBox.style.width = "60px";
+    blueBox.style.height = "60px";
+    blueBox.style.marginLeft = "1480px";
+    */
+    blueBox.style.marginLeft = "1480px";
+    blueBox.id = "obstacle" + numObs;
+    blueBox.style.marginTop = arrObs[numObs][1];
+
+
+    gameSection.appendChild(blueBox);
+    currentObs.push(document.getElementById("obstacle" + numObs))
+    numObs++;
+
+    if (numObs < arrObs.length) {
+        console.log(numObs);
+        console.log(arrObs[numObs][0]);
+        setTimeout(pushObstacle, arrObs[numObs][0]);
+    }
+}
 // UTILS //
+
 
 function pixToInt(pixels) {
     return Number(pixels.slice(0, pixels.length - 2));
+}
 
+function intToPix(integer) {
+    return integer + "px";
 }
 
 function position(element) {
-
     let h2 = pixToInt(element.style.height) / 2
     //let w2 = pixToInt(element.style.width) / 2
     return [element.offsetTop + h2, element.offsetLeft + h2, h2]
-
 }
 
 function size(element) {
-
     return [pixToInt(element.style.height), pixToInt(element.style.width)]
 }
 
 function collision(element1, element2) {
-
     p1 = position(element1);
     p2 = position(element2);
-
     if (Math.abs(p1[0] - p2[0]) <= (p1[2] + p2[2]) && Math.abs(p1[1] - p2[1]) <= (p1[2] + p2[2])) {
-
         console.log("collision!");
-
     }
 }
 
-function borderTop() {
-    return pixToInt(document.getElementById('myId').style.marginTop) > marginGame;
+function borderTop(element) {
+    return pixToInt(element.style.marginTop) > marginGame;
 }
 
-function borderBottom() {
-    return pixToInt(document.getElementById('myId').style.marginTop) < limitBottom;
+function borderBottom(element) {
+    return pixToInt(element.style.marginTop) < limitBottom;
 }
 
-function borderLeft() {
-    return pixToInt(document.getElementById('myId').style.marginLeft) > marginGame;
+function borderLeft(element) {
+    return pixToInt(element.style.marginLeft) > marginGame;
 }
 
-function borderRight() {
-    return pixToInt(document.getElementById('myId').style.marginLeft) < limitRight;
+function borderRight(element) {
+    return pixToInt(element.style.marginLeft) < limitRight;
 }
 
 
